76c460023ccf1da4cb5f785467a32a5bf44da39a
Ortes <malo.allee@gmail.com>
2025-06-25 23:38:36 +0200

    Fix poster assignment to ensure it is a string in VideoPlayer class

--- lib/src/video_player.dart
+++ lib/src/video_player.dart
@@ -437,7 +437,7 @@ class VideoPlayer {
     }
 
     if (options.poster != null) {
-      _videoElement.poster = options.poster!;
+      _videoElement.poster = options.poster!.toString();
     }
   }
 
